948feea1afd130e3dd48745a8122d75c613c070a
abelin540 <72109211+abelin540@users.noreply.github.com>
2020-09-29 18:15:50 -0500

    Add files via upload

---
+++ clases/Crud.php
@@ -0,0 +1,43 @@
+<?php 
+	require_once "Conexion.php";
+	class Crud extends Conectar {
+		public function mostrar() {
+			$conexion = Conectar::conexion();
+			$sql = "SELECT id_videojuego,
+		
+							nombre,
+							tipo,
+							fecha,
+							descripcion
+					FROM t_videojuegos";
+			$result = mysqli_query($conexion, $sql);
+
+			$cadena = "<tbody>";
+
+			while ($ver = mysqli_fetch_array($result)) {
+				$cadena = $cadena . '<tr>'.
+										'<td>' . $ver['id_videojuego']. '</td>
+										<td>' . $ver['nombre']. '</td>
+										<td> ' . $ver['tipo']. ' </td>
+										<td> ' . $ver['fecha']. ' </td>
+										<td> ' . $ver['descripcion']. ' </td>
+										<td>
+											<button class="btn btn-warning">
+												<span class="fas fa-user-edit">
+												</span>
+											</button>
+										</td>
+										<td>
+											<button class="btn btn-danger">
+												<span class="fas fa-user-times">
+												</span>
+											</button>
+										</td>'.
+									'</tr>';
+			}
+
+			return $cadena."</tbody>";
+		}
+	}
+
+ ?>
